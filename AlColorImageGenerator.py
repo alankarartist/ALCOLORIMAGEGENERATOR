1906f3dc45319bedb6e0600816879d20c99727fb
starboyalankarartist <alankarartist@gmail.com>
2021-03-10 16:47:19 +0530

    AlColorImageGenerator

--- AlColorImageGenerator.py
+++ AlColorImageGenerator.py
@@ -2,17 +2,20 @@ import numpy as np
 import cv2
 from tkinter import*
 from tkinter import font
+import os
+
+cwd = os.path.dirname(os.path.realpath(__file__))
 
 class AlColorImageGenerator:
     def __init__(self):
-        root = Tk(className=" AlColorImageGenerator ")
-        root.geometry("300x90+1600+925")
+        root = Tk(className=" ALCOLORIMAGENERATOR ")
+        root.geometry("350x90+1560+925")
         root.config(bg="#FFFFFF")
         color = '#FFFFFF'
 
         def convert():
-            net = cv2.dnn.readNetFromCaffe('./AlColorImageGenerator/model/colorization_deploy_v2.prototxt','./AlColorImageGenerator/model/colorization_release_v2.caffemodel')
-            pts = np.load('./AlColorImageGenerator/model/pts_in_hull.npy')
+            net = cv2.dnn.readNetFromCaffe(cwd+'\AlColorImageGenerator\model\colorization_deploy_v2.prototxt',cwd+'\AlColorImageGenerator\model\colorization_release_v2.caffemodel')
+            pts = np.load(cwd+'\AlColorImageGenerator\model\pts_in_hull.npy')
             class8 = net.getLayerId("class8_ab")
             conv8 = net.getLayerId("conv8_313_rh")
             pts = pts.transpose().reshape(2,313,1,1)
@@ -20,7 +23,8 @@ class AlColorImageGenerator:
             net.getLayer(conv8).blobs = [np.full([1,313],2.606,dtype='float32')]
             img = imageFile.get()
             cimg = 'color_' + img
-            img = './AlColorImageGenerator/images/bw/' + img
+            img = os.path.join(cwd+'\AlColorImageGenerator\images', 'bw\\'+img)
+            print(img)
             image = cv2.imread(img)
             scaled = image.astype("float32")/255.0
             lab = cv2.cvtColor(scaled,cv2.COLOR_BGR2LAB)
@@ -35,7 +39,7 @@ class AlColorImageGenerator:
             colorized = cv2.cvtColor(colorized,cv2.COLOR_LAB2BGR)
             colorized = np.clip(colorized,0,1)
             colorized = (255 * colorized).astype("uint8")
-            cimg = './AlColorImageGenerator/images/color/' + cimg
+            cimg = os.path.join(cwd+'\AlColorImageGenerator\images\color', cimg)
             cv2.imwrite(cimg, colorized) 
             cv2.imshow("Original",image)
             cv2.imshow("Colorized",colorized)
